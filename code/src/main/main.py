ff54d4084fe169a9bc7caec36d822badc363f95f
Florian Herrmann <herrmann.florian@mailbox.org>
2025-05-26 10:49:06 +0200

    Quick Test in main

--- code/src/main/main.py
+++ code/src/main/main.py
@@ -1,6 +1,7 @@
 from motor_driver import MotorDriver
 from logger import setup_logger
-from keyboard_control import keyboard_loop
+import time
+# from keyboard_control import keyboard_loop
 
 try:
     import RPi.GPIO as GPIO
@@ -15,7 +16,16 @@ if __name__ == "__main__":
     motor_driver = MotorDriver(left_motor_pins=(15,16), right_motor_pins=(18,22))
 
     try:
-        keyboard_loop(motor_driver, log)
+        # keyboard_loop(motor_driver, log)
+        motor_driver.forward()
+        time.sleep(5)
+        motor_driver.backward()
+        time.sleep(5)
+        motor_driver.rotate_left()
+        time.sleep(5)
+        motor_driver.rotate_right()
+        time.sleep(5)
+        motor_driver.stop()
     finally:
         motor_driver.cleanup()
         log.info("Cleaned up GPIO and exited.")
